4794c8d43179b9914145c1dc881a0b0112b6015e
amckeownwvnet <116903775+amckeownwvnet@users.noreply.github.com>
2023-09-26 09:32:40 -0400

    js fix
    fixed js issues with search

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
 var search = new Filter('search', 'data-caption');
 
-baguetteBox.run('.gallery'){
-        captions: true,
-        buttons: 'auto',
-        fullScreen: false,
-        noScrollbars: false,
-        titleTag: true,
-        async: false,
-        preload: 2,
-        animation: 'fadeIn',
-        onChange: null,
-        overlayBackgroundColor: 'rgba(113,117,115,0.8)'
-    };
-
+baguetteBox.run('.gallery')
